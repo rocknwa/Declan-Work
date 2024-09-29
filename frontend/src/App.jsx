8e20e73511f6447cfe648874b3fb19f13cc50989
Mohammed Owais <owaismohammed795@gmail.com>
2024-09-29 22:11:42 +0530

    Update: Added routes for job listings, job details, application form and post application form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignupPage from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
-
+import JobListings from './pages/JobListings';
+import JobDetails from './pages/JobDetails';
+import ApplyJobs from './pages/ApplyJobs';
+import AppSuccess from './pages/AppSuccess';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/" element={<Dashboard />} />
+        <Route path="/jobs" element={<JobListings />} />
+        <Route path="/jobs/:companyName" element={<JobListings />} />
+        <Route path="/jobs/:companyName/:jobId" element={<JobDetails />} />
+        <Route path="/jobs/:companyName/:jobId/apply" element={<ApplyJobs />} />
+        <Route path="/jobs/:companyName/:jobId/congrats" element={<AppSuccess />} />
       </Routes>
     </Router>
   );
