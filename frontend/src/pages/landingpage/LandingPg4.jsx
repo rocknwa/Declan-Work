d88af8f21e56a73c130d189ddd719bd470322f29
codetective <iaminiodu@gmail.com>
2024-10-20 07:05:32 +0100

    fix some responsive

--- frontend/src/pages/landingpage/LandingPg4.jsx
+++ frontend/src/pages/landingpage/LandingPg4.jsx
@@ -10,13 +10,16 @@ function LandingPg4() {
             <span className='text-4xl text-[#093218] font-bold'>How it works</span>
         </div>
         <HowItWorksNav selectedItem={selectedItem} setSelectedItem={setSelectedItem} />
-        <div className='flex justify- items-center text-[#093218] font-bold text-5xl w-full'>
-          <div className='w-7/12 p-4'>
+        <div className='flex flex-col md:flex-row justify-center items-center text-[#093218] font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl w-full'>
+          <div className='w-full lg:w-7/12 p-4'>
             {navItems.map((item) => item.title === selectedItem ? 
-            <div key={item.title} className='w-4/5 leading-[3rem]'>{item.content} </div> : '') }
+            <div key={item.title} className='w-full lg:w-4/5 leading-[1.5em] lg:leading-[3rem]'>{item.content}</div> : '')}
           </div>
-          <div className='w-5/12'>
-            {selectedItem === 'signup' ? <img src='/icons/sign-in-icon.svg' alt='Sign in icon' className='block max-w-full'/> : <img src="/icons/bitcoin-wallet.svg" alt="Bitcoin wallet logo" className='block max-w-full'/>}
+          <div className='w-full lg:w-5/12 mt-6 lg:mt-0'>
+            {selectedItem === 'signup' ? 
+              <img src='/icons/sign-in-icon.svg' alt='Sign in icon' className='block max-w-full mx-auto' /> : 
+              <img src="/icons/bitcoin-wallet.svg" alt="Bitcoin wallet logo" className='block max-w-full mx-auto' />
+            }
           </div>
         </div>
     </div>
