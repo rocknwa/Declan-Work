ea7660609178d12e39752517d70fe0b660338b7a
ekene <ekeneuduike@gmail.com>
2025-01-25 08:57:44 +0100

    added services for connecting to smart contract

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,12 @@
 // import Image from "next/image";
-import { HomePage } from "@/components/Home/Home";
-import { Footer } from "@/components/Shared/Footer/Footer";
+import { HomePage } from "../components/Home/Home";
+import { Footer } from "../components/Shared/Footer/Footer";
+
 
 export default function Home() {
   return (
-    <><HomePage /><Footer /></>
+    <>
+      <HomePage />
+      <Footer /></>
   );
 }
